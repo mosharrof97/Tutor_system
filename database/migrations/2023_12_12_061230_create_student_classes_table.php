c99fb8bffb98d4536ff2c26bace9f34f5435b941
tarikulat <tarikulat124@gmail.com>
2023-12-12 13:12:02 +0600

    model

---
+++ database/migrations/2023_12_12_061230_create_student_classes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_classes', function (Blueprint $table) {
+            $table->id('student_id');
+            $table->string('student_name');
+            $table->string('category_id');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_classes');
+    }
+};
